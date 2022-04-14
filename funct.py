1716afd13702afef6248d34f60af379b92e0d795
Ryan <rlmcmillan12@gmail.com>
2022-04-14 17:17:25 -0400

    fixed checker algorithm and edited README

--- funct.py
+++ funct.py
@@ -19,12 +19,14 @@ def two_dice():
 
 def shut_the_box_roll(master_list):
     if 7 not in master_list and 8 not in master_list and 9 not in master_list:
-        roll_choice = print(
+        roll_choice = input(
             "would you like to roll one or two dice?\nEnter 1 or 2: ")
         if roll_choice == "1":
             return one_die()
-        if roll_choice == "2":
+        elif roll_choice == "2":
             return two_dice()
+        else:
+            return shut_the_box_roll(master_list)
     elif 7 in master_list or 8 in master_list or 9 in master_list:
         print("You still have 7, 8, and 9 uncovered\nRolling two dice")
         return two_dice()
@@ -51,19 +53,19 @@ def cover_checker_list_maker(master_list):
     return cover_checker_list
 
 
-def cover_checker(roll_total, cover_checker_list):
-    filtered_list = cover_checker_list_maker(cover_checker_list)
-    return _cover_checker(roll_total, filtered_list, {})
-
-
-def _cover_checker(roll_total, cover_checker_list, memo):
-
-    if roll_total < 0:
+def cover_checker(roll_total: int, master_list: list[int], memo={}):
+    filtered_list = cover_checker_list_maker(master_list)
+    if (roll_total in memo):
+        return memo[roll_total]
+    if (roll_total < 0):
         return False
-    if roll_total == 0:
+    if (roll_total == 0):
         return True
-    for num in cover_checker_list:
-        if _cover_checker(roll_total - num, cover_checker_list[1:], memo) == True:
+
+    for i in range(len(filtered_list)):
+        if cover_checker(roll_total - filtered_list[i], filtered_list[i + 1:], memo):
+            memo[roll_total] = True
             return True
 
+    memo[roll_total] = False
     return False
