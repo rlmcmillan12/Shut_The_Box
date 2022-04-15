23ec440a04931d2017b554bfafb691d3315b9bc0
Ryan <rlmcmillan12@gmail.com>
2022-04-15 11:25:12 -0400

    cleared test file

--- test.py
+++
@@ -1,8 +0,0 @@
-from funct import cover_checker
-
-
-# print(cover_checker(3, [1, 2]))
-# print(cover_checker(6, [1, 2, 3]))
-# print(cover_checker(8, [1, 2, 6]))
-# print(cover_checker(9, [1, 2, 6]))
-print(cover_checker(10, [1, 2, 6]))
